eb9ff20359ced2b048ec4446a8668ac92de9f2fe
Mystic Striver <187439614+nosratee-jahan-naba@users.noreply.github.com.>
2026-03-16 17:23:59 +0600

    feature add

--- script/script.js
+++ script/script.js
@@ -17,3 +17,37 @@ function updateStats()
     document.getElementById('job-count').textContent = count === 1 ? '1 job' : count + ' jobs';
  
 }
+
+
+function getVisibleJobs(){
+    const store = [];
+    for (const job of jobs) {
+        if (currentTab === 'all' || job.status === currentTab)   store.push(job);
+    }
+    return store;
+}
+
+
+const  ACTIVE_CLASS = 'text-sm font-medium px-4 py-2 rounded bg-blue-600 text-white transition-all'
+const  INACTIVE_CLASS = 'text-sm font-medium px-4 py-2 rounded text-gray-600 border border-gray-300 hover:bg-gray-100  transition-all'
+
+
+function switchTab(tab){
+    currentTab = tab;
+
+    document.getElementById('tab-btn-all').className = tab === 'all'    ?  ACTIVE_CLASS : INACTIVE_CLASS;
+    document.getElementById('tab-btn-interview').className = tab === 'interview'    ?  ACTIVE_CLASS : INACTIVE_CLASS;
+    document.getElementById('tab-btn-rejected').className = tab === 'rejected'    ?  ACTIVE_CLASS : INACTIVE_CLASS;
+
+    render();
+}
+function setStatus(id, status){
+    for (const job of jobs) 
+    {
+        if (job.id === id ) {
+            job.status = job.status === status ? 'none' : status ; 
+            break;
+        }
+    }
+    render();
+}
